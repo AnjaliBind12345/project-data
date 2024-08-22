01d7a9673dd376f889a73d3a4c2bfe8a94e5c745
AnjaliBind12345 <anbind3724@gmail.com>
2024-08-22 23:51:30 +0530

    Added environment variable

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 8000;
+const jsonServer = require("json-server");
+const server = create();
+const router = router('db.json');
+const middlewares = defaults();
 
 server.use(middlewares);
 server.use(router);
-server.listen(port, () => {
-  console.log(`JSON Server is running on port ${port}`);
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on port ${PORT}`);
 });
+
